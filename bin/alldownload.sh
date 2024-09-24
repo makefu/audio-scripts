36350e0b130fd3401f68043ad6be574bd4d8a0f3
makefu <github@syntax-fehler.de>
2024-09-25 01:22:34 +0200

    alldownload: shorten name

--- bin/alldownload.sh
+++ bin/alldownload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 HERE="$(dirname "$(readlink -f "$0")")"
 OUTDIR=${1:-/media/silent/music/kinder/podcasts}
-"$HERE/gendownload.sh" "$OUTDIR/checkpod"  "CheckPod - Der Podcast mit Checker Tobi"  'https://feeds.br.de/checkpod-der-podcast-mit-checker-tobi/feed.xml'
+"$HERE/gendownload.sh" "$OUTDIR/checkpod"  "CheckPod"  'https://feeds.br.de/checkpod-der-podcast-mit-checker-tobi/feed.xml'
 "$HERE/gendownload.sh" "$OUTDIR/die.maus.zum.hoeren" "Die Maus zum Hören"  'https://kinder.wdr.de/radio/diemaus/audio/diemaus-60/diemaus-60-106.podcast'
 #./gendownload.sh "$OUTDIR/purplus" "Purplus Podcast"  'https://purplus-podcast.podigee.io/feed/mp3'
