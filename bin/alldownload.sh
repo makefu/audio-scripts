5ff394180e52a081bbca0d21c18794edbf1dbea6
makefu <github@syntax-fehler.de>
2024-09-25 22:58:35 +0200

    add two more br podcasts

--- bin/alldownload.sh
+++ bin/alldownload.sh
@@ -3,4 +3,6 @@ HERE="$(dirname "$(readlink -f "$0")")"
 OUTDIR=${1:-/media/silent/music/kinder/podcasts}
 "$HERE/gendownload.sh" "$OUTDIR/checkpod"  "CheckPod"  'https://feeds.br.de/checkpod-der-podcast-mit-checker-tobi/feed.xml'
 "$HERE/gendownload.sh" "$OUTDIR/die.maus.zum.hoeren" "Die Maus zum Hören"  'https://kinder.wdr.de/radio/diemaus/audio/diemaus-60/diemaus-60-106.podcast'
+"$HERE/gendownload.sh" "$OUTDIR/anna.und.die.wilden.tiere" "Anna und die wilden Tiere"  'https://feeds.br.de/anna-und-die-wilden-tiere/feed.xml'
+"$HERE/gendownload.sh" "$OUTDIR/lachlabor" "Lachlabor"  'https://feeds.br.de/lachlabor/feed.xml'
 #./gendownload.sh "$OUTDIR/purplus" "Purplus Podcast"  'https://purplus-podcast.podigee.io/feed/mp3'
